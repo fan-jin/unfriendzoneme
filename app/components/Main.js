82f7895ba0db0b18908ecb6c76b43a3200662e1d
Fan Jin <im.fan.jin@gmail.com>
2016-06-01 19:48:46 -0400

    Add form with input

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import Form from './Form';
 
 class Main extends React.Component {
     render() {
-        return <h2 className="text-center">
-          You got friendzoned? No Problem.
-        </h2>;
+        return (
+          <div>
+            <h2 className="text-center">
+              You got friendzoned? No Problem.
+            </h2>
+            <Form />
+          </div>
+        );
     }
 }
 
--- public/bundle.js
+++ public/bundle.js
@@ -20334,11 +20334,65 @@
 /* 168 */
 /***/ function(module, exports, __webpack_require__) {
 
-	"use strict";
+	'use strict';
 
 	Object.defineProperty(exports, "__esModule", {
-	    value: true
+	  value: true
 	});
+
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+	var _react = __webpack_require__(1);
+
+	var _react2 = _interopRequireDefault(_react);
+
+	var _Form = __webpack_require__(169);
+
+	var _Form2 = _interopRequireDefault(_Form);
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+	var Main = function (_React$Component) {
+	  _inherits(Main, _React$Component);
+
+	  function Main() {
+	    _classCallCheck(this, Main);
+
+	    return _possibleConstructorReturn(this, Object.getPrototypeOf(Main).apply(this, arguments));
+	  }
+
+	  _createClass(Main, [{
+	    key: 'render',
+	    value: function render() {
+	      return _react2.default.createElement(
+	        'div',
+	        null,
+	        _react2.default.createElement(
+	          'h2',
+	          { className: 'text-center' },
+	          'You got friendzoned? No Problem.'
+	        ),
+	        _react2.default.createElement(_Form2.default, null)
+	      );
+	    }
+	  }]);
+
+	  return Main;
+	}(_react2.default.Component);
+
+	exports.default = Main;
+
+/***/ },
+/* 169 */
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
 
 	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
@@ -20354,30 +20408,24 @@
 
 	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-	var Main = function (_React$Component) {
-	    _inherits(Main, _React$Component);
+	var Form = function (_React$Component) {
+	  _inherits(Form, _React$Component);
 
-	    function Main() {
-	        _classCallCheck(this, Main);
+	  function Form() {
+	    _classCallCheck(this, Form);
 
-	        return _possibleConstructorReturn(this, Object.getPrototypeOf(Main).apply(this, arguments));
+	    return _possibleConstructorReturn(this, Object.getPrototypeOf(Form).apply(this, arguments));
+	  }
+
+	  _createClass(Form, [{
+	    key: "render",
+	    value: function render() {
+	      return _react2.default.createElement("input", { type: "text" });
 	    }
+	  }]);
 
-	    _createClass(Main, [{
-	        key: "render",
-	        value: function render() {
-	            return _react2.default.createElement(
-	                "h2",
-	                { className: "text-center" },
-	                "You got friendzoned? No Problem."
-	            );
-	        }
-	    }]);
-
-	    return Main;
+	  return Form;
 	}(_react2.default.Component);
-
-	exports.default = Main;
 
 /***/ }
 /******/ ]);

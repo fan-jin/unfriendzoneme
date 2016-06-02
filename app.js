f5ddbbef76660ddbcc2014364760cf0bfea975b6
Fan Jin <im.fan.jin@gmail.com>
2016-06-01 21:28:05 -0400

    Accept post at /msg

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var bodyParser = require('body-parser')
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.post('/msg', function(req, res){
+  console.log(req.body.msg);
+});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));

34a48ceea8bf9acc77aa92a0d56af05b927f49b1
Fan Jin <im.fan.jin@gmail.com>
2016-06-11 16:27:42 -0400

    Save email credentials to ENV

--- app.js
+++ app.js
@@ -3,8 +3,19 @@ var app = express();
 var path = require('path');
 var bodyParser = require('body-parser')
 var nodemailer = require('nodemailer');
+
+var smtpConfig = {
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true, // use SSL
+    auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD
+    }
+};
+
 // create reusable transporter object using the default SMTP transport
-var transporter = nodemailer.createTransport('smtps://unfriendzoneme%40gmail.com:unfriendzoned@smtp.gmail.com');
+var transporter = nodemailer.createTransport(smtpConfig);
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -20,8 +31,8 @@ app.post('/msg', function(req, res){
   res.sendStatus(200); // default success status
   // setup e-mail data with unicode symbols
   var mailOptions = {
-      from: email + '<unfriendzoneme@gmail.com>', // sender address
-      to: 'unfriendzoneme@gmail.com', // list of receivers
+      from: email + '<' + process.env.EMAIL + '>', // sender address
+      to: process.env.EMAIL, // list of receivers
       subject: 'I need advice', // Subject line
       text: msg, // plaintext body
       html: msg // html body
